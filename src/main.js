8d6fc317ae454b4160343399b6eb23d74353d1e8
Octansito <octanecodlol@gmail.com>
2025-10-19 13:30:42 +0200

    chore: init Vite vanilla limpio

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <main style="font-family: system-ui; padding: 2rem">
+    <h1>Vanilla + Vite ✅</h1>
+    <p>Entorno listo y HMR funcionando.</p>
+  </main>
+`;
